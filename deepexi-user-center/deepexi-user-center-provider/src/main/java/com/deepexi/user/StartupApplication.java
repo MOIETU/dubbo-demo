671b95570d35ec05d1587b8b37c380e7f568acac
mojinchao <297513931@qq.com>
2019-05-14 16:36:59 +0800

    支持Dubbo

--- deepexi-user-center/deepexi-user-center-provider/src/main/java/com/deepexi/user/StartupApplication.java
+++ deepexi-user-center/deepexi-user-center-provider/src/main/java/com/deepexi/user/StartupApplication.java
@@ -1,8 +1,10 @@
 package com.deepexi.user;
 
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableDubbo(scanBasePackages="com.deepexi.user")
 @SpringBootApplication
 public class StartupApplication {
 
