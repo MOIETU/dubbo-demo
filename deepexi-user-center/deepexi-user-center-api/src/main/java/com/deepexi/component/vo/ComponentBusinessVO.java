01505ab7a2444e376d737969097145150a3c581c
mojinchao <297513931@qq.com>
2019-05-14 14:51:28 +0800

    添加组件VO

--- deepexi-user-center/deepexi-user-center-api/src/main/java/com/deepexi/component/vo/ComponentBusinessVO.java
+++ deepexi-user-center/deepexi-user-center-api/src/main/java/com/deepexi/component/vo/ComponentBusinessVO.java
@@ -1,5 +1,106 @@
 package com.deepexi.component.vo;
 
-public class ComponentBusinessVO {
+import java.util.Date;
 
+public class ComponentBusinessVO {
+    /** 数据ID */
+    private Long id;
+    /** 组件名称 */
+    private String name;
+    /** 组件类型ID */
+    private Long typeId;
+    /** 类型名称 */
+    private String typeName;
+    /** 版本 */
+    private String version;
+    /** 状态 */
+    private Integer status;
+    /** 创建用户 */
+    private Long createUser;
+    /** 创建日期 */
+    private Date createDate;
+    /** 更新用户 */
+    private Long updateUser;
+    /** 更新日期 */
+    private Date updateDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+
+    public Long getTypeId() {
+        return typeId;
+    }
+
+    public void setTypeId(Long typeId) {
+        this.typeId = typeId;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Long getCreateUser() {
+        return createUser;
+    }
+
+    public void setCreateUser(Long createUser) {
+        this.createUser = createUser;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Long getUpdateUser() {
+        return updateUser;
+    }
+
+    public void setUpdateUser(Long updateUser) {
+        this.updateUser = updateUser;
+    }
+
+    public Date getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
+    }
 }
